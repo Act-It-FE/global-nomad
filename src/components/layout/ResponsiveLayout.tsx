010ed628d874bc119ec4ed4b0b11d30efb288445
two678 <two6788@gmail.com>
2025-07-16 16:31:07 +0900

    ✨ feat: pc, tablet 사이즈일때도 가운데로 설정
    Resolves: #5

--- src/components/layout/ResponsiveLayout.tsx
+++ src/components/layout/ResponsiveLayout.tsx
@@ -16,23 +16,27 @@ export default function ResponsiveLayout({
   // 모바일 + /mypage 루트일 때만 SideMenu만 보여줌
   if (isMobile && pathname === '/mypage') {
     return (
-      <div className='flex items-center justify-center'>
+      <div className='mt-35 flex items-center justify-center'>
         <SideMenu />
       </div>
     );
   } else if (isMobile) {
     return (
-      <div className='mx-24 flex items-center justify-center'>{children}</div>
+      <div className='mx-24 mt-30 flex items-center justify-center'>
+        {children}
+      </div>
     );
   }
 
   // 그 외에는 children도 같이 보여줌
   return (
-    <div className='mt-40 flex w-full max-w-980 items-start justify-between min-md:mx-30 min-md:gap-30'>
-      <SideMenu />
-      <main className='flex w-640 flex-col items-start justify-center'>
-        {children}
-      </main>
+    <div className='flex items-center justify-center'>
+      <div className='mt-40 flex w-full max-w-980 items-start justify-between min-md:mx-30 min-md:gap-30'>
+        <SideMenu />
+        <main className='flex w-640 flex-col items-start justify-center'>
+          {children}
+        </main>
+      </div>
     </div>
   );
 }

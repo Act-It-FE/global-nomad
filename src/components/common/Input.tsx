f6d2e1076cb86f5b8eb301261145db0e2e023b21
Moon-ju-young <amymoon07@khu.ac.kr>
2025-07-16 16:02:35 +0900

    ✨ feat: InputProps 및 관련 type 추가
    InputType, CommonProps, InputProps type

--- src/components/common/Input.tsx
+++ src/components/common/Input.tsx
@@ -1,3 +1,39 @@
-export default function Input() {
+import { InputHTMLAttributes } from 'react';
+
+type InputType =
+  | 'button'
+  | 'checkbox'
+  | 'color'
+  | 'date'
+  | 'datetime-local'
+  | 'email'
+  | 'file'
+  | 'hidden'
+  | 'image'
+  | 'month'
+  | 'number'
+  | 'password'
+  | 'radio'
+  | 'range'
+  | 'reset'
+  | 'search'
+  | 'submit'
+  | 'tel'
+  | 'text'
+  | 'time'
+  | 'url'
+  | 'week';
+
+interface CommonProps {
+  id: string;
+  label?: string;
+  errorMessage?: string;
+}
+
+type InputProps = CommonProps & {
+  type?: InputType;
+} & InputHTMLAttributes<HTMLInputElement>;
+
+export default function Input({}: InputProps) {
   return;
 }

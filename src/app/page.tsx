155ab92d828bdcc675cb21afa34d46e534e38e4e
two678 <two6788@gmail.com>
2025-07-16 13:58:02 +0900

    🐛 fix: 코드래빗 요구사항 수정
    Resolves: #20

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,9 @@
 import Icon from '@/components/common/Icon';
 import ICON_MAP from '@/constants/iconMap';
-import { cn } from '@/utils/cn';
-
 export default function Home() {
   const iconKeys = Object.keys(ICON_MAP) as (keyof typeof ICON_MAP)[];
   return (
-    <div className={cn('grid grid-cols-5', 'gap-4')}>
+    <div className='grid grid-cols-5 gap-4'>
       {iconKeys.map((iconKey) => (
         <div key={iconKey} className='flex flex-col items-center'>
           <Icon className='text-primary-500 size-20' icon={iconKey} />

9518d97ed87055f733ffe41d577d1804831b6724
Moon-ju-young <amymoon07@khu.ac.kr>
2025-07-17 03:09:39 +0900

    ✨ feat: useClickOutside 로직 구현
    Resolves: #31
    Related to: #7

--- src/hooks/useClickOutside.ts
+++ src/hooks/useClickOutside.ts
@@ -1,3 +1,19 @@
-export function useClickOutside() {
+import { RefObject, useEffect } from 'react';
+
+export function useClickOutside<T extends HTMLElement = HTMLElement>(
+  ref: RefObject<T>,
+  handler: (e: MouseEvent) => void,
+) {
+  useEffect(() => {
+    const handleOutsideClick = (e: MouseEvent) => {
+      if (ref.current && !ref.current.contains(e.target as Node)) handler(e);
+    };
+
+    document.addEventListener('click', handleOutsideClick);
+    return () => {
+      document.removeEventListener('click', handleOutsideClick);
+    };
+  });
+
   return;
 }

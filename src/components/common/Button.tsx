fc1046949ef458b08a5dea48b10fe51d4a08bdab
seong5 <greenbi0852@gmail.com>
2025-07-18 19:04:20 +0900

    ♻️ refactor:  불필요한 타입 분리 삭제 한군데서만 사용하는 타입의 경우 컴포넌트 파일에 정리하는것이 맞다고 판단되어 옮기고 불필요한 파일을 삭제했습니다.

--- src/components/common/Button.tsx
+++ src/components/common/Button.tsx
@@ -1,9 +1,23 @@
+import { ReactNode } from 'react';
+
 import {
   BUTTON_ROUNDED,
   BUTTON_SIZE,
   BUTTON_VARIANTS,
+  buttonRoundedPixel,
+  buttonVariants,
 } from '@/constants/ButtonStyles';
-import { ButtonProps } from '@/types/Button';
+
+export interface ButtonProps {
+  children: ReactNode;
+  className?: string;
+  size?: keyof typeof BUTTON_SIZE;
+  variant?: buttonVariants;
+  rounded?: buttonRoundedPixel;
+  onClick?: (e: React.MouseEvent<HTMLButtonElement>) => void;
+  type?: 'button' | 'submit' | 'reset';
+  icon?: ReactNode;
+}
 
 export default function Button({
   children,
@@ -18,7 +32,7 @@ export default function Button({
   const sizeStyle = size ? BUTTON_SIZE[size] : ''; //사이즈 Prop 을 사용하지 않으면 className 으로 스타일 지정 가능
   return (
     <button
-      className={` ${BUTTON_VARIANTS[variant]} ${BUTTON_ROUNDED[rounded]} ${sizeStyle} transition ${className} `}
+      className={`${BUTTON_VARIANTS[variant]} ${BUTTON_ROUNDED[rounded]} ${sizeStyle} transition ${className} `}
       type={type}
       onClick={onClick}
     >

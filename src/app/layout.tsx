9c066e098e0ac166ea0dab80a1335b1c4c127395
dain <dainbabo@naver.com>
2025-07-18 14:37:21 +0900

    Rename footer.tsx to Footer.tsx to match import path

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import './globals.css';
 
 import localFont from 'next/font/local';
 
-import Footer from '@/components/common/Footer';
+import Footer from '@/components/common/footer';
 
 const pretendard = localFont({
   src: '../../public/fonts/PretendardVariable.woff2',

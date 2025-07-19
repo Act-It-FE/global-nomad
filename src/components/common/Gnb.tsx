1e8a2138ddc950150580d65f47e13452c407ec4c
Moon-ju-young <amymoon07@khu.ac.kr>
2025-07-19 23:02:37 +0900

    ♻️ refactor: nav 태그 하나로만 구성 변경
    Resolves: #38

--- src/components/common/Gnb.tsx
+++ src/components/common/Gnb.tsx
@@ -1,7 +1,5 @@
 export default function Gnb() {
   return (
-    <nav>
-      <div className='mx-auto flex h-48 max-w-1580 items-center justify-between px-24 md:h-80 md:px-30' />
-    </nav>
+    <nav className='mx-auto flex h-48 max-w-1580 items-center justify-between px-24 md:h-80 md:px-30' />
   );
 }

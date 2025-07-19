3dbb185a4334425ee92c2fb2574f9043639e119d
seong5 <greenbi0852@gmail.com>
2025-07-19 16:34:44 +0900

    ✨ 드롭다운 컴포넌트 수정 클릭에 필요한 아이콘들을 받아와서 열리게 하는 기능과 드롭다운이 열리는 위치를 따로 지정했습니다. Resolve: #39

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,18 @@
 'use client';
 
 import DropDown from '@/components/common/DropDown';
+import Icon from '@/components/common/Icon';
+
 export default function Home() {
   return (
-    <div>
+    <div className='m-20 flex flex-col items-center justify-center'>
       <DropDown
         firstText='마이페이지'
+        position='bottom'
         secondText='로그아웃'
+        trigger={<Icon className='h-28 w-28' icon='More' />}
         onClickFirst={() => console.log('마이페이지')}
         onClickSecond={() => console.log('로그아웃')}
-      />
-      <DropDown
-        firstText='수정하기'
-        secondText='삭제하기'
-        onClickFirst={() => console.log('수정하기')}
-        onClickSecond={() => console.log('삭제하기')}
       />
     </div>
   );
--- src/components/common/DropDown.tsx
+++ src/components/common/DropDown.tsx
@@ -1,20 +1,28 @@
 'use client';
 
+import { ReactNode, useState } from 'react';
+
 import { cn } from '@/utils/cn';
 
 type DropDownProps = {
+  trigger: ReactNode;
   firstText: string;
   secondText: string;
   onClickFirst: () => void;
   onClickSecond: () => void;
+  position?: 'bottom' | 'left';
 };
 
 export default function DropDown({
+  trigger,
   firstText,
   secondText,
   onClickFirst,
   onClickSecond,
+  position = 'bottom',
 }: DropDownProps) {
+  const [isOpen, setIsOpen] = useState(false);
+
   const buttonClass = cn(
     'cursor-pointer py-[18px] hover:bg-primary-100 w-full max-h-55 txt-16_M',
   );
@@ -22,14 +30,36 @@ export default function DropDown({
   const textClass = (text: string) => {
     return cn('text-gray-950', text === '로그아웃' && 'text-red-500');
   };
+
+  const dropdownPosition = cn(
+    'absolute z-50',
+    position === 'left'
+      ? 'right-full mr-2 top-1/2 -translate-y-1/5'
+      : 'top-full mt-2 left-1/2 -translate-x-1/2',
+  );
   return (
-    <div className='flex w-95 flex-col justify-center rounded-[8px] border border-gray-50 bg-white text-gray-950 md:h-106 md:w-103 lg:h-auto lg:w-95'>
-      <button className={buttonClass} onClick={onClickFirst}>
-        <span>{firstText}</span>
-      </button>
-      <button className={buttonClass} onClick={onClickSecond}>
-        <span className={textClass(secondText)}>{secondText}</span>
-      </button>
+    <div className='relative'>
+      <div
+        className='cursor-pointer'
+        onClick={() => setIsOpen((prev) => !prev)}
+      >
+        {trigger}
+      </div>
+      {isOpen && (
+        <div
+          className={cn(
+            dropdownPosition,
+            'flex w-95 flex-col justify-center rounded-[8px] border border-gray-50 bg-white text-gray-950 md:h-106 md:w-103 lg:h-auto lg:w-95',
+          )}
+        >
+          <button className={buttonClass} onClick={onClickFirst}>
+            <span>{firstText}</span>
+          </button>
+          <button className={buttonClass} onClick={onClickSecond}>
+            <span className={textClass(secondText)}>{secondText}</span>
+          </button>
+        </div>
+      )}
     </div>
   );
 }

a6163ae0fff3022b4a055644bc64f05406b86c84
seong5 <greenbi0852@gmail.com>
2025-07-19 13:49:11 +0900

    ✨ feat: 드롭다운 컴포넌트 드롭다운 컴포넌트를 제작했습니다. 추가 수정할 부분들을 고려해보고 추가 커밋하겠습니다. Resolve: #39

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,21 @@
+'use client';
+
 import DropDown from '@/components/common/DropDown';
 export default function Home() {
-  return <DropDown />;
+  return (
+    <div>
+      <DropDown
+        firstText='마이페이지'
+        secondText='로그아웃'
+        onClickFirst={() => console.log('마이페이지')}
+        onClickSecond={() => console.log('로그아웃')}
+      />
+      <DropDown
+        firstText='수정하기'
+        secondText='삭제하기'
+        onClickFirst={() => console.log('수정하기')}
+        onClickSecond={() => console.log('삭제하기')}
+      />
+    </div>
+  );
 }
--- src/components/common/DropDown.tsx
+++ src/components/common/DropDown.tsx
@@ -1,9 +1,35 @@
-import Icon from './Icon';
+'use client';
 
-export default function DropDown() {
+import { cn } from '@/utils/cn';
+
+type DropDownProps = {
+  firstText: string;
+  secondText: string;
+  onClickFirst: () => void;
+  onClickSecond: () => void;
+};
+
+export default function DropDown({
+  firstText,
+  secondText,
+  onClickFirst,
+  onClickSecond,
+}: DropDownProps) {
+  const buttonClass = cn(
+    'cursor-pointer py-[18px] hover:bg-primary-100 w-full max-h-55 txt-16_M',
+  );
+
+  const textClass = (text: string) => {
+    return cn('text-gray-950', text === '로그아웃' && 'text-red-500');
+  };
   return (
-    <button>
-      <Icon className='size-28 cursor-pointer' icon='More' />
-    </button>
+    <div className='flex w-95 flex-col justify-center rounded-[8px] border border-gray-50 bg-white text-gray-950 md:h-106 md:w-103 lg:h-auto lg:w-95'>
+      <button className={buttonClass} onClick={onClickFirst}>
+        <span>{firstText}</span>
+      </button>
+      <button className={buttonClass} onClick={onClickSecond}>
+        <span className={textClass(secondText)}>{secondText}</span>
+      </button>
+    </div>
   );
 }

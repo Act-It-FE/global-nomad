f2fff26c85567e6243e131effe6ae1826dae4d9c
Moon-ju-young <amymoon07@khu.ac.kr>
2025-07-19 22:56:42 +0900

    ✨ feat: nav 내부 div 태그 추가
    Resolves: #38

--- src/components/common/Gnb.tsx
+++ src/components/common/Gnb.tsx
@@ -1,3 +1,7 @@
 export default function Gnb() {
-  return <nav className='h-48 md:h-80' />;
+  return (
+    <nav>
+      <div className='mx-auto flex h-48 max-w-1580 items-center justify-between px-24 md:h-80 md:px-30' />
+    </nav>
+  );
 }

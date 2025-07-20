a4ec0eb0c6b5e885ccab74c2104b34bf5c2f127c
Moon-ju-young <amymoon07@khu.ac.kr>
2025-07-20 16:28:14 +0900

    ✨ feat: logo Image 추가
    Resolves: #38

--- src/components/common/Gnb.tsx
+++ src/components/common/Gnb.tsx
@@ -1,5 +1,22 @@
+import Image from 'next/image';
+
 export default function Gnb() {
   return (
-    <nav className='mx-auto flex h-48 max-w-1580 items-center justify-between px-24 md:h-80 md:px-30' />
+    <nav className='mx-auto flex h-48 max-w-1580 items-center justify-between px-24 md:h-80 md:px-30'>
+      <Image
+        alt='logo'
+        className='max-md:hidden'
+        height={28}
+        src='/images/logo-sm-text.png'
+        width={174}
+      />
+      <Image
+        alt='logo'
+        className='md:hidden'
+        height={28}
+        src='/images/logo-sm.png'
+        width={28}
+      />
+    </nav>
   );
 }

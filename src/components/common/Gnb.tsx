4a6d20ca21832cdbfe3f2a50819630bbae136258
Moon-ju-young <amymoon07@khu.ac.kr>
2025-07-20 16:34:37 +0900

    ✨ feat: Image Link 추가
    Resolves: #38

--- src/components/common/Gnb.tsx
+++ src/components/common/Gnb.tsx
@@ -1,22 +1,25 @@
 import Image from 'next/image';
+import Link from 'next/link';
 
 export default function Gnb() {
   return (
     <nav className='mx-auto flex h-48 max-w-1580 items-center justify-between px-24 md:h-80 md:px-30'>
-      <Image
-        alt='logo'
-        className='max-md:hidden'
-        height={28}
-        src='/images/logo-sm-text.png'
-        width={174}
-      />
-      <Image
-        alt='logo'
-        className='md:hidden'
-        height={28}
-        src='/images/logo-sm.png'
-        width={28}
-      />
+      <Link href='/'>
+        <Image
+          alt='logo'
+          className='max-md:hidden'
+          height={28}
+          src='/images/logo-sm-text.png'
+          width={174}
+        />
+        <Image
+          alt='logo'
+          className='md:hidden'
+          height={28}
+          src='/images/logo-sm.png'
+          width={28}
+        />
+      </Link>
     </nav>
   );
 }

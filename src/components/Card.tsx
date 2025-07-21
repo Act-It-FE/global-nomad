1dc3d83606b7d64087bc8de1d31fb374def59c66
Yun-Jinwoo <yjw3480@naver.com>
2025-07-21 23:33:15 +0900

    ✨ feat: absolute 방식 relative로 변경

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -6,17 +6,16 @@ import Icon from './common/Icon';
 export default function Card({ image, title, rating, reviewCount, price, id }) {
   const formatted = price.toLocaleString();
   return (
-    <Link
-      className='relative aspect-[155/177] w-full md:aspect-[332/347] lg:h-290 lg:w-262'
-      href={`/${id}`}
-    >
-      <Image
-        fill
-        alt={`${title} 이미지`}
-        className='rounded-[18px] object-cover md:rounded-[32px]'
-        src={image}
-      />
-      <article className='absolute bottom-[-66px] flex w-full flex-col gap-4 rounded-[18px] bg-white px-17 py-16 text-gray-950 shadow-[-4.5px_-4.5px_11.25px_0px_rgba(0,0,0,0.05)] md:bottom-[-76px] md:gap-2 md:rounded-[32px] md:px-30 md:py-20'>
+    <Link className='flex w-full flex-col lg:w-262' href={`/${id}`}>
+      <div className='relative aspect-[155/177] w-full md:aspect-[332/347] lg:h-290'>
+        <Image
+          fill
+          alt={`${title} 이미지`}
+          className='rounded-[18px] object-cover md:rounded-[32px]'
+          src={image}
+        />
+      </div>
+      <article className='relative z-10 mt-[-66px] flex w-full flex-col gap-4 rounded-[18px] bg-white px-17 py-16 text-gray-950 shadow-[-4.5px_-4.5px_11.25px_0px_rgba(0,0,0,0.05)] md:mt-[-76px] md:gap-2 md:rounded-[32px] md:px-30 md:py-20'>
         <h3 className='truncate text-[14px]/18 font-semibold md:text-[18px]/26'>
           {title}
         </h3>

5e3a63f75944298c28964586df4197ce570108f2
Moon-ju-young <amymoon07@khu.ac.kr>
2025-07-22 15:40:46 +0900

    ✨ feat: Pagination 추가
    Resolves: #60

--- src/app/mypage/test/page.tsx
+++ src/app/mypage/test/page.tsx
@@ -1,3 +1,27 @@
+'use client';
+
+import { useState } from 'react';
+
+import Pagination from '@/components/common/Pagination';
+
 export default function Page() {
-  return;
+  const [page1, setPage1] = useState(1);
+  const [page2, setPage2] = useState(1);
+
+  return (
+    <article className='flex flex-col items-center p-40'>
+      <Pagination
+        currentPage={page1}
+        pageSize={5}
+        totalCount={320}
+        onPageChange={(p) => setPage1(p)}
+      />
+      <Pagination
+        currentPage={page2}
+        pageSize={5}
+        totalCount={15}
+        onPageChange={(p) => setPage2(p)}
+      />
+    </article>
+  );
 }

8c92772f3975a99cfd6dce0bc110ce073253c20d
Moon-ju-young <amymoon07@khu.ac.kr>
2025-07-22 15:56:47 +0900

    ✨ feat: h2 태그 및 외부 containter div 설정
    Resolves: #60

--- src/app/mypage/test/page.tsx
+++ src/app/mypage/test/page.tsx
@@ -9,19 +9,22 @@ export default function Page() {
   const [page2, setPage2] = useState(1);
 
   return (
-    <article className='flex flex-col items-center p-40'>
-      <Pagination
-        currentPage={page1}
-        pageSize={5}
-        totalCount={320}
-        onPageChange={(p) => setPage1(p)}
-      />
-      <Pagination
-        currentPage={page2}
-        pageSize={5}
-        totalCount={15}
-        onPageChange={(p) => setPage2(p)}
-      />
-    </article>
+    <div className='flex flex-col gap-20 p-40'>
+      <h2 className='txt-20_B'>Pagination</h2>
+      <article className='flex flex-col items-center'>
+        <Pagination
+          currentPage={page1}
+          pageSize={5}
+          totalCount={320}
+          onPageChange={(p) => setPage1(p)}
+        />
+        <Pagination
+          currentPage={page2}
+          pageSize={5}
+          totalCount={15}
+          onPageChange={(p) => setPage2(p)}
+        />
+      </article>
+    </div>
   );
 }

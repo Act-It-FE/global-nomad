4319594105601a865d9e0363680a35a4a870e024
dain <dainbabo@naver.com>
2025-07-22 16:08:20 +0900

    ✨ feat: 알림 승인/거절 색상
    react-string-replace 설치해 처리했습니다
    
    Resolves:#42

--- src/components/common/Notification.tsx
+++ src/components/common/Notification.tsx
@@ -1,5 +1,7 @@
 'use client';
 
+import stringReplace from 'react-string-replace';
+
 import Icon from '@/components/common/Icon';
 
 export interface Notification {
@@ -24,11 +26,24 @@ function NotificationPanel({ open, onClose, list }: NotificationPanelProps) {
     if (diff < 86400) return `${Math.floor(diff / 3600)}시간 전`;
     return `${Math.floor(diff / 86400)}일 전`;
   };
+  const highlight = (text: string) =>
+    stringReplace(text, /(승인|거절)/g, (match, i) => (
+      <strong
+        key={i}
+        className={
+          match === '승인'
+            ? 'text-primary-500 txt-14_M'
+            : 'txt-14_M text-red-500'
+        }
+      >
+        {match}
+      </strong>
+    ));
 
   if (!open) return null;
 
   return (
-    <div className='absolute top-[27px] right-[-116px] z-50 flex h-326 w-327 flex-col rounded-[10px] bg-white shadow-lg sm:right-0 sm:w-231'>
+    <div className='absolute top-[27px] right-[-116px] z-50 flex h-[326px] w-[327px] flex-col rounded-[10px] bg-white shadow-lg sm:right-0 sm:w-[231px]'>
       <div className='flex items-center justify-between border-b border-gray-100 px-20 py-16'>
         <span className='txt-16_B'>알림 {totalCount}개</span>
         <button onClick={onClose}>
@@ -41,7 +56,8 @@ function NotificationPanel({ open, onClose, list }: NotificationPanelProps) {
 
       <ul className='flex-1 overflow-y-auto [scrollbar-width:none]'>
         {list.map((n) => {
-          const [title, ...rest] = n.content.split('\n');
+          const [title, activityName, schedule, ...restLines] =
+            n.content.split('\n');
           return (
             <li
               key={n.id}
@@ -53,9 +69,15 @@ function NotificationPanel({ open, onClose, list }: NotificationPanelProps) {
                   {timeAgo(n.createdAt)}
                 </span>
               </div>
-              {rest.length > 0 && (
-                <p className='txt-14_body_M text-gray-800'>{rest.join(' ')}</p>
-              )}
+              <div>
+                <p className='txt-14_body_M text-gray-800'>{activityName}</p>
+                <p className='txt-14_body_M text-gray-800'>{schedule}</p>
+                {restLines.length > 0 && (
+                  <p className='txt-14_body_M text-gray-800'>
+                    {highlight(restLines.join(' '))}
+                  </p>
+                )}
+              </div>
             </li>
           );
         })}

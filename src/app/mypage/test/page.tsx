473389a6352baf3015820d156f37d74d09954836
Moon-ju-young <amymoon07@khu.ac.kr>
2025-07-22 16:20:11 +0900

    ✨ feat: Input 추가
    Resolves: #60

--- src/app/mypage/test/page.tsx
+++ src/app/mypage/test/page.tsx
@@ -2,6 +2,7 @@
 
 import { useState } from 'react';
 
+import Input from '@/components/common/Input';
 import Pagination from '@/components/common/Pagination';
 
 export default function Page() {
@@ -9,7 +10,39 @@ export default function Page() {
   const [page2, setPage2] = useState(1);
 
   return (
-    <div className='flex flex-col gap-20 p-40'>
+    <div className='flex w-full flex-col gap-40 p-40'>
+      <h2 className='txt-20_B'>Input</h2>
+      <article className='flex flex-col gap-20'>
+        <Input id='text1' label='text' placeholder='placeholder' />
+        <Input errorMessage='error' id='text2' />
+        <Input
+          id='password1'
+          label='password'
+          placeholder='placeholder'
+          type='password'
+        />
+        <Input errorMessage='error' id='password2' type='password' />
+        <Input
+          height='120px'
+          id='textarea1'
+          label='textarea'
+          placeholder='placeholder'
+          type='textarea'
+        />
+        <Input
+          errorMessage='error'
+          height='120px'
+          id='textarea2'
+          type='textarea'
+        />
+        <Input
+          id='dropdown'
+          items={['one', 'two']}
+          label='dropdown'
+          placeholder='placeholder'
+          type='dropdown'
+        />
+      </article>
       <h2 className='txt-20_B'>Pagination</h2>
       <article className='flex flex-col items-center'>
         <Pagination

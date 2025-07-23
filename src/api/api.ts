4e4806db000a76a03d241a8a0124df0c7046e3c6
dain <dainbabo@naver.com>
2025-07-23 16:34:29 +0900

    ✏️  fix: env type설정

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosInstance } from 'axios';
 
 const instance: AxiosInstance = axios.create({
-  baseURL: process.env.VITE_API_BASE_URL,
+  baseURL: import.meta.env.VITE_API_BASE_URL,
   timeout: 3000,
   headers: {
     'Content-Type': 'application/json',

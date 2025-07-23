cd30a4644c082ef2e45a85e8c95ba860c724945e
Moon-ju-young <amymoon07@khu.ac.kr>
2025-07-24 05:30:35 +0900

    ✨ feat: Category type 추가
    Resolves: #56

--- src/types/MyActivities.ts
+++ src/types/MyActivities.ts
@@ -1,4 +1,13 @@
 // refactoring 필요
+export type Category =
+  | '문화 · 예술'
+  | '식음료'
+  | '스포츠'
+  | '투어'
+  | '관광'
+  | '웰빙';
+
+// refactoring 필요
 // ReservationResponse, ReservationWithActivityResponse 에 사용
 export type ReservationStatus =
   | 'pending'

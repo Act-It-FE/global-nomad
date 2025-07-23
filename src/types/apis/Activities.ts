b1973af393c4a28c924a80778ee38ea6fe36c490
seong5 <greenbi0852@gmail.com>
2025-07-23 17:37:31 +0900

    ✨ feat: 체험상세 조회 api 타입지정 체험 상세조회의 api 응답값에 대한 타입을 지정했습니다. Resolve: #61

--- src/types/apis/Activities.ts
+++ src/types/apis/Activities.ts
@@ -1,4 +1,6 @@
 // Activities 타입
+
+// 체험 리스트 조회
 export interface Activity {
   id: number;
   userId: number;
@@ -16,4 +18,22 @@ export interface Activity {
 
 export interface ActivityResponse {
   activities: Activity[];
+}
+
+// 체험 상세 조회
+export interface ActivitiesDetail extends Activity {
+  subImages: SubImage[];
+  schedules: Schedule[];
+}
+
+export interface SubImage {
+  id: number;
+  imageUrl: string;
+}
+
+export interface Schedule {
+  id: number;
+  date: string;
+  startTime: string;
+  endTime: string;
 }

85d36fd9bb0b2d3e833a915a0602e09a23f3a0bf
seong5 <greenbi0852@gmail.com>
2025-07-25 10:06:58 +0900

    ✨ feat: 체험 리뷰 api 체험 리뷰를 가져오는 api 함수를 만들고 isAxiosError를 통해 에러처리를 했습니다. Resolve: #61

--- src/libs/activitiesDetail.ts
+++ src/libs/activitiesDetail.ts
@@ -1,18 +1,20 @@
-import axios from 'axios';
+import { isAxiosError } from 'axios';
 
-import { ActivitiesDetail } from '@/types/apis/Activities';
+import { fetcher } from '@/api/api';
+import {
+  ActivitiesDetail,
+  ActivityReviewResponse,
+} from '@/types/apis/Activities';
 
+// 체험 상세 조회
 export const getActivityDetail = async (
-  teamId: string,
   activityId: number,
 ): Promise<ActivitiesDetail> => {
   try {
-    const response = await axios.get(
-      `https://sp-globalnomad-api.vercel.app/${teamId}/activities/${activityId}`,
-    );
+    const response = await fetcher.get(`/activities/${activityId}`);
     return response.data;
   } catch (error) {
-    if (axios.isAxiosError(error)) {
+    if (isAxiosError(error)) {
       const status = error.response?.status;
 
       if (status === 404) {
@@ -26,5 +28,22 @@ export const getActivityDetail = async (
 
     console.error('네트워크 에러 발생:', error);
     throw new Error('네트워크 에러가 발생했습니다.');
+  }
+};
+
+// 체험 리뷰 조회
+export const getActivityReviews = async (
+  activityId: number,
+): Promise<ActivityReviewResponse> => {
+  try {
+    const response = await fetcher.get(`/activities/${activityId}/reviews`);
+    return response.data;
+  } catch (error) {
+    if (isAxiosError(error)) {
+      console.error('체험 리뷰 조회에 실패했습니다.', error.message);
+    } else {
+      console.error('알 수 없는 오류가 발생했습니다.', error);
+    }
+    throw new Error('체험 리뷰 조회 중 오류가 발생했습니다.');
   }
 };

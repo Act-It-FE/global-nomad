6a3e7073ee2eea774f99b4fad1b0cb25d1311323
two678 <two6788@gmail.com>
2025-07-25 21:06:04 +0900

    ✨ feat : auth api 구현
    auth api 구현
     api 관련 타입 위치 변경 및 일부 추가
    apiClient 추가
    Resolves: #77

---
+++ src/api/authApi.ts
@@ -0,0 +1,12 @@
+import apiClient from '@/libs/apiClient';
+
+import { LoginRequest, LoginResponse, Token } from './types/Auth';
+
+export const authApi = {
+  login: (data: LoginRequest) => {
+    return apiClient.post<LoginRequest, LoginResponse>('/auth/login', data);
+  },
+  getTokens: (data: Token) => {
+    return apiClient.post<Token, Token>('/auth/tokens', data);
+  },
+};

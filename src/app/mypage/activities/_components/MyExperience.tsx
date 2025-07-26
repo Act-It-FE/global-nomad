9e6478fc32a4746eec3a895be4a6ee59d5e2356c
Moon-ju-young <amymoon07@khu.ac.kr>
2025-07-27 00:13:31 +0900

    ✨ feat: img 요소 추가
    추후 Image로 변경 필요
    
    Resolves: #120

--- src/app/mypage/activities/_components/MyExperience.tsx
+++ src/app/mypage/activities/_components/MyExperience.tsx
@@ -1,7 +1,7 @@
 import { ActivityBasic } from '@/types/MyActivities';
 import { cn } from '@/utils/cn';
 
-export default function MyExperience({}: { data: ActivityBasic }) {
+export default function MyExperience({ data }: { data: ActivityBasic }) {
   return (
     <article
       className={cn(
@@ -9,6 +9,13 @@ export default function MyExperience({}: { data: ActivityBasic }) {
         'rounded-3xl p-24 lg:p-30',
         'card-shadow bg-white',
       )}
-    />
+    >
+      <div />
+      <img //Image로 변경 필요
+        alt='banner image'
+        className='size-82 rounded-[20px] object-cover lg:size-142 lg:rounded-[30px]'
+        src={data.bannerImageUrl}
+      />
+    </article>
   );
 }

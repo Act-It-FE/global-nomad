013564215b32d92842679eae119ec9bff1aaec0a
Yun-Jinwoo <yjw3480@naver.com>
2025-07-27 00:31:19 +0900

    🎨 style: 텍스트 크기 설정 변경

--- src/app/_components/Search.tsx
+++ src/app/_components/Search.tsx
@@ -6,8 +6,8 @@ export default function Search() {
   const isMobile = useMediaQuery('mobile');
 
   return (
-    <div className='txt-16_B flex flex-col items-center gap-12 py-16'>
-      <h1>무엇을 체험하고 싶으신가요?</h1>
+    <div className='flex flex-col items-center gap-12 py-16'>
+      <h1 className='txt-16_B'>무엇을 체험하고 싶으신가요?</h1>
       <div className='flex w-full items-center justify-center gap-8 rounded-[16px] bg-white py-6 pr-8 pl-20 shadow-[0_6px_10px_0_rgba(13,153,255,0.05)]'>
         <div className='flex flex-1 gap-4'>
           <Icon className='size-24 text-gray-950' icon='Search' />

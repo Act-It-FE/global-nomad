d3fa80119234e9f7cf50f511eacf456c5208a389
Moon-ju-young <amymoon07@khu.ac.kr>
2025-07-26 18:42:51 +0900

    ♻️ refactor: 함수 그대로 return 하도록 변경
    Resolves: #84

--- src/api/myActivities.ts
+++ src/api/myActivities.ts
@@ -13,18 +13,17 @@ import {
 import { fetcher } from '../libs/api';
 
 /** 내 체험 리스트 조회 */
-export const getMyActivities = async (query?: {
+export const getMyActivities = (query?: {
   cursorId?: number;
   size?: number;
 }) => {
-  const response = await fetcher.get<ActivitiesResponse>('/my-activities', {
+  return fetcher.get<ActivitiesResponse>('/my-activities', {
     params: query,
   });
-  return response.data;
 };
 
 /** 내 체험 월별 예약 현황 조회 */
-export const getMyActivitiesReservationDashboard = async (
+export const getMyActivitiesReservationDashboard = (
   activityId: number,
   query: {
     /** YYYY 형식 (예시: 2025) */
@@ -33,34 +32,32 @@ export const getMyActivitiesReservationDashboard = async (
     month: string;
   },
 ) => {
-  const response = await fetcher.get<FindReservationsByMonthResponse>(
+  return fetcher.get<FindReservationsByMonthResponse>(
     `/my-activities/${activityId}/reservation-dashboard`,
     {
       params: query,
     },
   );
-  return response.data;
 };
 
 /** 내 체험 날짜별 예약 정보(신청, 승인, 거절)가 있는 스케줄 조회 */
-export const getMyActivitiesReservedSchedule = async (
+export const getMyActivitiesReservedSchedule = (
   activityId: number,
   query: {
     /** YYYY-MM-DD 형식 (예시: 2025-07-26) */
     date: string;
   },
 ) => {
-  const response = await fetcher.get<ReservedScheduleResponse>(
+  return fetcher.get<ReservedScheduleResponse>(
     `/my-activities/${activityId}/reserved-schedule`,
     {
       params: query,
     },
   );
-  return response.data;
 };
 
 /** 내 체험 예약 시간대별 예약 내역 조회 */
-export const getMyActivitiesReservations = async (
+export const getMyActivitiesReservations = (
   activityId: number,
   query: {
     cursorId?: number;
@@ -69,42 +66,38 @@ export const getMyActivitiesReservations = async (
     status: Extract<'declined' | 'pending' | 'confirmed', ReservationStatus>;
   },
 ) => {
-  const response = await fetcher.get<ReservationsListResponse>(
+  return fetcher.get<ReservationsListResponse>(
     `/my-activities/${activityId}/reservations`,
     {
       params: query,
     },
   );
-  return response.data;
 };
 
 /** 내 체험 예약 상태(승인, 거절) 업데이트 */
-export const patchMyActivitiesReservations = async (
+export const patchMyActivitiesReservations = (
   activityId: number,
   reservationId: number,
   body: UpdateMyActivityReservationBody,
 ) => {
-  const response = await fetcher.patch<ReservationResponse>(
+  return fetcher.patch<ReservationResponse>(
     `/my-activities/${activityId}/reservations/${reservationId}`,
     body,
   );
-  return response.data;
 };
 
 /** 내 체험 삭제 */
-export const deleteMyActivities = async (activityId: number) => {
-  const response = await fetcher.delete<void>(`/my-activities/${activityId}`);
-  return response.data;
+export const deleteMyActivities = (activityId: number) => {
+  return fetcher.delete<void>(`/my-activities/${activityId}`);
 };
 
 /** 내 체험 수정 */
-export const patchMyActivities = async (
+export const patchMyActivities = (
   activityId: number,
   body: UpdateMyActivityBody,
 ) => {
-  const response = await fetcher.patch<ActivityWithSchedulesResponse>(
+  return fetcher.patch<ActivityWithSchedulesResponse>(
     `/my-activities/${activityId}`,
     body,
   );
-  return response.data;
 };

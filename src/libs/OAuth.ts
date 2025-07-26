086d63eda5f3ce52cdb6058fb07728392410f794
dain <dainbabo@naver.com>
2025-07-27 01:15:08 +0900

    ♻️ refactor: apiClient사용 코드로 수정

--- src/libs/OAuth.ts
+++ src/libs/OAuth.ts
@@ -7,43 +7,28 @@ import {
   OAuthSignUpRequest,
 } from '@/types/Auth';
 
-import { fetcher } from './api';
+import apiClient from './apiClient';
 
-export const postApp = async (
-  body: OAuthAppRequest,
-): Promise<OAuthAppResponse> => {
-  try {
-    const response = await fetcher.post<OAuthAppResponse>('/oauth/apps', body);
-    return response.data;
-  } catch {
-    throw new Error('error');
-  }
-};
-export const postSignUp = async (
-  body: OAuthSignUpRequest,
-  provider: OAuthAppProvider,
-): Promise<OAuthResponse> => {
-  try {
-    const response = await fetcher.post<OAuthResponse>(
+const oAuthApi = {
+  postApp: (body: OAuthAppRequest): Promise<OAuthAppResponse> =>
+    apiClient.post<OAuthAppRequest, OAuthAppResponse>('/oauth/apps', body),
+
+  postSignUp: (
+    body: OAuthSignUpRequest,
+    provider: OAuthAppProvider,
+  ): Promise<OAuthResponse> =>
+    apiClient.post<OAuthSignUpRequest, OAuthResponse>(
       `/oauth/sign-up/${provider}`,
       body,
-    );
-    return response.data;
-  } catch {
-    throw new Error('간편 회원 가입에 실패하였습니다..');
-  }
-};
-export const postLogin = async (
-  body: OAuthLoginRequest,
-  provider: OAuthAppProvider,
-): Promise<OAuthResponse> => {
-  try {
-    const response = await fetcher.post<OAuthResponse>(
+    ),
+
+  postLogin: (
+    body: OAuthLoginRequest,
+    provider: OAuthAppProvider,
+  ): Promise<OAuthResponse> =>
+    apiClient.post<OAuthLoginRequest, OAuthResponse>(
       `/oauth/sign-in/${provider}`,
       body,
-    );
-    return response.data;
-  } catch {
-    throw new Error('간편 로그인에 실패하였습니다.');
-  }
+    ),
 };
+export default oAuthApi;

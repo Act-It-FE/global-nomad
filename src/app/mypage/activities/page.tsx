82e1b551c5bab13eac26fd5321ccc0f5a2ddbeb9
Moon-ju-young <amymoon07@khu.ac.kr>
2025-07-28 04:18:06 +0900

    ✨ feat: 페이지 제목 문구 추가
    Resolves: #104

--- src/app/mypage/activities/page.tsx
+++ src/app/mypage/activities/page.tsx
@@ -2,7 +2,9 @@ export default function page() {
   return (
     <div className='flex w-full flex-col gap-15 md:gap-30'>
       <header className='flex items-center justify-between gap-12'>
-        <div className='flex flex-col gap-10 py-10' />
+        <div className='flex flex-col gap-10 py-10'>
+          <div className='txt-18_B text-gray-950'>내 체험 관리</div>
+        </div>
       </header>
     </div>
   );

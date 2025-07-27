b04ae32c53a4031f5388362b11734dfab5d9d122
seong5 <greenbi0852@gmail.com>
2025-07-27 16:52:41 +0900

    ♻️ refactor: export default 추가

--- src/libs/activitiesDetail.ts
+++ src/libs/activitiesDetail.ts
@@ -13,73 +13,74 @@ import {
 } from '@/api/types/Activities';
 import apiClient from '@/libs/apiClient';
 
-//체험 리스트 조회
-export const getActivities = async (
-  query?: GetActivitiesParams,
-): Promise<ActivityResponse> => {
-  return await apiClient.get(`/activities`, {
-    params: query,
-  });
+const activitiesDetailApi = {
+  /** 체험 리스트 조회 */
+  get: (query?: GetActivitiesParams) => {
+    return apiClient.get<ActivityResponse>(`/activities`, {
+      params: query,
+    });
+  },
+
+  /** 체험 상세 조회 */
+  getDetail: (activityId: number) => {
+    return apiClient.get<ActivitiesDetail>(`/activities/${activityId}`);
+  },
+
+  /** 체험 리뷰 조회 */
+  getReviews: (activityId: number, query?: GetActivityReviewsParams) => {
+    return apiClient.get<ActivityReviewResponse>(
+      `/activities/${activityId}/reviews`,
+      {
+        params: query,
+      },
+    );
+  },
+
+  /** 체험 예약 가능일 조회 */
+  getAvailableSchedule: (
+    activityId: number,
+    query: GetAvailableSchduleParams,
+  ) => {
+    const formattedQuery = {
+      ...query,
+      month: query.month.length === 1 ? `0${query.month}` : query.month,
+    };
+    return apiClient.get<AvailableSchedule[]>(
+      `/activities/${activityId}/available-schedule`,
+      {
+        params: formattedQuery,
+      },
+    );
+  },
+
+  /** 체험 등록 */
+  post: (payload: ActivityRegisterPayload) => {
+    return apiClient.post(`/activities`, payload);
+  },
+
+  /** 체험 예약 신청 */
+  postReservation: (activityId: number, payload: ReservationRequest) => {
+    return apiClient.post<ReservationRequest, ReservationResponse>(
+      `/activities/${activityId}/reservations`,
+      payload,
+    );
+  },
+
+  /** 체험 이미지 업로드 */
+  uploadImage: (file: File) => {
+    const formData = new FormData();
+    formData.append('image', file);
+
+    return apiClient.post<FormData, ActivityImageUploadResponse>(
+      `/activities/image`,
+      formData,
+      {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+        },
+      },
+    );
+  },
 };
 
-// 체험 상세 조회
-export const getActivityDetail = async (
-  activityId: number,
-): Promise<ActivitiesDetail> => {
-  return await apiClient.get(`/activities/${activityId}`);
-};
-
-// 체험 리뷰 조회
-export const getActivityReviews = async (
-  activityId: number,
-  query?: GetActivityReviewsParams,
-): Promise<ActivityReviewResponse> => {
-  return await apiClient.get(`/activities/${activityId}/reviews`, {
-    params: query,
-  });
-};
-
-//체험 예약 가능일 조회
-export const getAvailableSchedule = async (
-  activityId: number,
-  query: GetAvailableSchduleParams,
-): Promise<AvailableSchedule[]> => {
-  const formattedQuery = {
-    ...query,
-    month: query.month.length === 1 ? `0${query.month}` : query.month,
-  };
-  return await apiClient.get(`/activities/${activityId}/available-schedule`, {
-    params: formattedQuery,
-  });
-};
-
-// 체험 등록
-export const createActivity = async (
-  payload: ActivityRegisterPayload,
-): Promise<void> => {
-  return await apiClient.post(`/activities`, payload);
-};
-
-// 체험 예약 신청
-export const postReservation = async (
-  activityId: number,
-  payload: ReservationRequest,
-): Promise<ReservationResponse> => {
-  return await apiClient.post(
-    `/activities/${activityId}/reservations`,
-    payload,
-  );
-};
-
-//체험 이미지 URL 생성
-export const uploadActivityImage = async (
-  file: File,
-): Promise<ActivityImageUploadResponse> => {
-  const formData = new FormData();
-  formData.append('image', file);
-  return await apiClient.post(`/activities/image`, formData, {
-    headers: {
-      'Content-Type': 'multipart/form-data',
-    },
-  });
-};
+export default activitiesDetailApi;

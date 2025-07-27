1504cd0890a2ea3893a7948552d474549353b187
Moon-ju-young <amymoon07@khu.ac.kr>
2025-07-28 04:31:09 +0900

    🎨 style: line-height 스타일 추가
    Resolves: #104

--- src/app/mypage/activities/page.tsx
+++ src/app/mypage/activities/page.tsx
@@ -3,8 +3,8 @@ export default function page() {
     <div className='flex w-full flex-col gap-15 md:gap-30'>
       <header className='flex items-center justify-between gap-12'>
         <div className='flex flex-col gap-10 py-10'>
-          <div className='txt-18_B text-gray-950'>내 체험 관리</div>
-          <div className='txt-14_M text-gray-500'>
+          <div className='txt-18_B leading-21 text-gray-950'>내 체험 관리</div>
+          <div className='txt-14_M leading-17 text-gray-500'>
             체험을 등록하거나 수정 및 삭제가 가능합니다.
           </div>
         </div>

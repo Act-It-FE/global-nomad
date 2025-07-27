efed4cfb58ad1c08b0973bd7a807737ebb3456c4
Moon-ju-young <amymoon07@khu.ac.kr>
2025-07-28 04:11:17 +0900

    ✨ feat: header 텍스트 부분 div 추가
    Resolves: #104

--- src/app/mypage/activities/page.tsx
+++ src/app/mypage/activities/page.tsx
@@ -1,7 +1,9 @@
 export default function page() {
   return (
     <div className='flex w-full flex-col gap-15 md:gap-30'>
-      <header className='flex items-center justify-between gap-12' />
+      <header className='flex items-center justify-between gap-12'>
+        <div className='flex flex-col gap-10 py-10' />
+      </header>
     </div>
   );
 }

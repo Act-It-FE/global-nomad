38570d1d3c712bb5407c310e2bd10c787c2789d4
Moon-ju-young <amymoon07@khu.ac.kr>
2025-07-28 04:01:11 +0900

    🐛 fix: style 오류 사항 수정
    Resolves: #104

--- src/app/mypage/activities/page.tsx
+++ src/app/mypage/activities/page.tsx
@@ -1,6 +1,6 @@
 export default function page() {
   return (
-    <div className='flex w-full flex-30 flex-col'>
+    <div className='flex w-full flex-col gap-15 md:gap-30'>
       <header className='flex items-center justify-between gap-12' />
     </div>
   );

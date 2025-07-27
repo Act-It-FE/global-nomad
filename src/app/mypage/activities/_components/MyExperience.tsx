1b82380b1a319b4968a98eacc8f05620a537b557
Moon-ju-young <amymoon07@khu.ac.kr>
2025-07-27 22:29:11 +0900

    ✨ feat: 가격 price 추가
    Resolves: #120

--- src/app/mypage/activities/_components/MyExperience.tsx
+++ src/app/mypage/activities/_components/MyExperience.tsx
@@ -19,6 +19,14 @@ export default function MyExperience({ data }: { data: ActivityBasic }) {
           <Icon className='text-yellow size-14 lg:size-16' icon='Star' />
           <div className='txt-13_M lg:txt-16_M leading-16 text-gray-500 lg:leading-19'>{`${data.rating} (${data.reviewCount})`}</div>
         </div>
+        <div className='mt-10 flex items-center gap-4 lg:mt-12'>
+          <div className='txt-16_B lg:txt-18_B leading-19 lg:leading-21'>
+            ￦{data.price.toLocaleString('ko-KR')}
+          </div>
+          <div className='txt-14_M lg:txt-16_M leading-17 text-gray-400 lg:leading-19'>
+            / 인
+          </div>
+        </div>
       </div>
       <img //Image로 변경 필요
         alt='banner image'

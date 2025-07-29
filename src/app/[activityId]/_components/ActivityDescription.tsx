3430443a98347efd6827dd99b27f3d2bcd9898e2
seong5 <greenbi0852@gmail.com>
2025-07-29 20:18:43 +0900

    ✨ feat: subImageUrl 렌더링 swagger 로 직접 체험 등록을 해서 이미지 3장을 모두 렌더링 했습니다. Resolve: #153

--- src/app/[activityId]/_components/ActivityDescription.tsx
+++ src/app/[activityId]/_components/ActivityDescription.tsx
@@ -11,6 +11,7 @@ type Props = {
 export default function ActivityDescription({ activityId }: Props) {
   const [description, setDescription] = useState('');
   const [bannerImageUrl, setBannerImageUrl] = useState('');
+  const [subImages, setSubImages] = useState<string[]>([]);
   const [errorMessage, setErrorMessage] = useState('');
   const [isLoading, setIsLoading] = useState(false);
 
@@ -24,6 +25,7 @@ export default function ActivityDescription({ activityId }: Props) {
 
         setDescription(activity.description);
         setBannerImageUrl(activity.bannerImageUrl);
+        setSubImages(activity.subImages.map((img) => img.imageUrl));
       } catch (error) {
         console.error('체험 설명을 불러오는데 실패했습니다.', error);
         setErrorMessage('체험 설명을 불러오지 못했습니다.');
@@ -44,14 +46,34 @@ export default function ActivityDescription({ activityId }: Props) {
   }
 
   return (
-    <section className='my-40 flex flex-col gap-16'>
-      {bannerImageUrl && (
-        <img
-          alt='체험 배너 이미지'
-          className='max-h-450 w-full rounded-[24px]'
-          src={bannerImageUrl}
-        />
-      )}
+    <section className='my-40 flex flex-col gap-24'>
+      <div className='grid grid-cols-1 gap-12 md:grid-cols-3'>
+        {bannerImageUrl && (
+          <div className='col-span-1 md:col-span-2'>
+            <img
+              alt='체험 배너 이미지'
+              className='h-full w-full rounded-tl-[24px] rounded-bl-[24px] object-cover'
+              src={bannerImageUrl}
+            />
+          </div>
+        )}
+
+        <div className='flex flex-col gap-12'>
+          {[0, 1].map((index) =>
+            subImages[index] ? (
+              <img
+                key={index}
+                alt={`서브 이미지 ${index + 1}`}
+                className={`h-full w-full object-cover ${
+                  index === 0 ? 'rounded-tr-[24px]' : 'rounded-br-[24px]'
+                }`}
+                src={subImages[index]}
+              />
+            ) : null,
+          )}
+        </div>
+      </div>
+
       <div className='md:txt-18_B txt-16_B'>체험 설명</div>
       <p className='txt-16_M whitespace-pre-line text-gray-950'>
         {description}

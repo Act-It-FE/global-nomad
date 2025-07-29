11538f35ef2a44265e9d4b841ce33c65c3eb371e
Yun-Jinwoo <yjw3480@naver.com>
2025-07-29 17:10:12 +0900

    ✨ feat: 이미지 없거나 불러올 수 없을때 fallback이미지 보여줌

--- src/app/_components/CardColumn.tsx
+++ src/app/_components/CardColumn.tsx
@@ -1,4 +1,3 @@
-import Image from 'next/image';
 import Link from 'next/link';
 
 import Icon from '../../components/Icon';
@@ -24,11 +23,14 @@ export default function CardColumn({
   return (
     <Link className='flex w-full flex-col lg:w-262' href={`/${id}`}>
       <div className='relative aspect-[155/177] w-full md:aspect-[332/347] lg:h-290'>
-        <Image
-          fill
+        {/* eslint-disable-next-line @next/next/no-img-element */}
+        <img
           alt={`${title} 이미지`}
-          className='rounded-[18px] object-cover md:rounded-[32px]'
-          src={bannerImageUrl}
+          className='h-full w-full rounded-[18px] object-cover md:rounded-[32px]'
+          src={bannerImageUrl || '/images/logo-lg.png'}
+          onError={(e) => {
+            e.currentTarget.src = '/images/logo-lg.png';
+          }}
         />
       </div>
       <article className='relative z-10 mt-[-66px] flex w-full flex-col gap-4 rounded-[18px] bg-white px-17 py-16 text-gray-950 shadow-[-4.5px_-4.5px_11.25px_0px_rgba(0,0,0,0.05)] md:mt-[-76px] md:gap-2 md:rounded-[32px] md:px-30 md:py-20'>

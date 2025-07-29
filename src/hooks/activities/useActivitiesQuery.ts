218f074a90c844ea349e67b04d7444a9b140ae20
Yun-Jinwoo <yjw3480@naver.com>
2025-07-29 19:46:05 +0900

    ✨ feat: 재배포를 위한 주석 수정

--- src/hooks/activities/useActivitiesQuery.ts
+++ src/hooks/activities/useActivitiesQuery.ts
@@ -7,6 +7,6 @@ export default function useActivitiesQuery(params: GetActivitiesParams) {
   return useQuery({
     queryKey: ['activities', params],
     queryFn: () => activitiesDetailApi.get(params),
-    staleTime: 0, // 실시간 반영을 위해 0초로 설정
+    staleTime: 0, // 실시간 반영을 위해 0초로 설정!
   });
 }

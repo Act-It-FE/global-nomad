e8e6726eb73e0fa9c699de6659e1876898410b4f
Yun-Jinwoo <yjw3480@naver.com>
2025-07-29 15:41:50 +0900

    ✨ feat: Activities Query 훅 구현

---
+++ src/hooks/activities/useActivitiesQuery.ts
@@ -0,0 +1,13 @@
+import { useQuery } from '@tanstack/react-query';
+
+import activitiesDetailApi from '@/api/activitiesApi';
+import { GetActivitiesParams } from '@/api/types/activities';
+
+export default function useActivitiesQuery(params: GetActivitiesParams) {
+  return useQuery({
+    queryKey: ['activities', params],
+    queryFn: () => activitiesDetailApi.get(params),
+    staleTime: 0, // 실시간 반영을 위해 0초로 설정
+    select: (data) => data.activities,
+  });
+}

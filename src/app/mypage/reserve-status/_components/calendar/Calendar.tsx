5ed925f1acf804af1bc62c001ac03b4cc60d9fcf
two678 <two6788@gmail.com>
2025-07-30 11:37:31 +0900

    ♻️ refactor : 캘린더 css 수정
    Resolves: #150

--- src/app/mypage/reserve-status/_components/calendar/Calendar.tsx
+++ src/app/mypage/reserve-status/_components/calendar/Calendar.tsx
@@ -4,7 +4,7 @@ import { CalendarHeader } from './CalendarHeader';
 
 export function Calendar() {
   return (
-    <div className='flex w-full flex-col gap-8 rounded-3xl bg-white pt-20 pb-10 shadow-[0_4px_24px_0_rgba(156,180,202,0.20)] md:gap-30'>
+    <div className='card-shadow flex w-full flex-col gap-8 rounded-3xl bg-white pt-20 pb-10 md:gap-30'>
       <CalendarHeader />
 
       <CalendarGrid />

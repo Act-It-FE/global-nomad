5075783534e5e513c29685482688c4f71676dbd8
Yun-Jinwoo <yjw3480@naver.com>
2025-07-30 16:12:56 +0900

    ✨ feat: 메인 페이지에 검색 로직 구현

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,33 @@
 'use client';
+import { useRouter, useSearchParams } from 'next/navigation';
+import { useState } from 'react';
 
 import AllActivities from './_components/AllActivities';
+import Search from './_components/Search';
+import SearchResult from './_components/SearchResult';
 
 export default function Home() {
+  const [isLoading, setIsLoading] = useState(false);
+
+  const searchParams = useSearchParams();
+  const router = useRouter();
+  const keyword = searchParams.get('search') || '';
+
+  const handleSearch = (query: string) => {
+    if (query.trim() === '') return;
+    const encoded = encodeURIComponent(query);
+    router.push(`/?search=${encoded}`);
+  };
+
   return (
     <div className='mx-auto max-w-1200'>
       <div className='mx-24 min-h-800 md:mx-30 lg:mx-40'>
-        <AllActivities />
+        <Search isLoading={isLoading} onSearch={handleSearch} />
+        {keyword ? (
+          <SearchResult keyword={keyword} onLoadingChange={setIsLoading} />
+        ) : (
+          <AllActivities />
+        )}
       </div>
     </div>
   );

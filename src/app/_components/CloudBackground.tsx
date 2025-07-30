aa2390f0a80a769c2c562255536e1552b192f313
Yun-Jinwoo <yjw3480@naver.com>
2025-07-30 22:14:10 +0900

    🎨 style: 메인페이지 배경 CloudBackground 구현

---
+++ src/app/_components/CloudBackground.tsx
@@ -0,0 +1,77 @@
+'use client';
+import Image from 'next/image';
+
+export default function CloudBackground({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div
+      className='relative overflow-hidden'
+      style={{
+        background: 'linear-gradient(180deg, #BDF 0%, #F7FBFF 30%, #FFF 100%)',
+      }}
+    >
+      {/* 구름 이미지들 */}
+      <Image
+        alt='구름'
+        className='absolute top-[2%] right-[25%]'
+        height={27}
+        src='/images/cloud.png'
+        width={71}
+      />
+      <Image
+        alt='구름'
+        className='absolute top-[6%] right-[10%]'
+        height={52}
+        src='/images/cloud.png'
+        width={138}
+      />
+      <Image
+        alt='구름'
+        className='absolute top-[18%] right-[5%] hidden md:block'
+        height={27}
+        src='/images/cloud.png'
+        width={71}
+      />
+      <Image
+        alt='구름'
+        className='absolute top-[9%] left-[50%]'
+        height={40}
+        src='/images/cloud.png'
+        width={105}
+      />
+      <Image
+        alt='구름'
+        className='absolute top-[2.5%] left-[15%]'
+        height={40}
+        src='/images/cloud.png'
+        width={105}
+      />
+      <Image
+        alt='구름'
+        className='absolute top-[8%] left-[5%]'
+        height={40}
+        src='/images/cloud.png'
+        width={105}
+      />
+      <Image
+        alt='구름'
+        className='absolute top-[15%] left-[-3%] hidden md:block'
+        height={40}
+        src='/images/cloud.png'
+        width={105}
+      />
+      <Image
+        alt='구름'
+        className='absolute top-[20%] left-[10%] hidden lg:block'
+        height={40}
+        src='/images/cloud.png'
+        width={105}
+      />
+
+      <div>{children}</div>
+    </div>
+  );
+}

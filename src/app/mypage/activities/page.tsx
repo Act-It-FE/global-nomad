329a64b2d031a7b4b5df455bd751701a678ebb12
Moon-ju-young <amymoon07@khu.ac.kr>
2025-07-31 16:12:45 +0900

    🎨 style: gap 간격 수정
    Resolves: #104

--- src/app/mypage/activities/page.tsx
+++ src/app/mypage/activities/page.tsx
@@ -13,7 +13,7 @@ export default function Page() {
   const { data } = useMyActQuery();
 
   return (
-    <div className='flex w-full flex-col gap-15 md:gap-30'>
+    <div className='flex w-full flex-col gap-30'>
       <header className='flex items-center justify-between gap-12'>
         <div className='flex flex-col gap-10 py-10'>
           <div className='txt-18_B leading-21 text-gray-950'>내 체험 관리</div>

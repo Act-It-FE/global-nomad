d6d7e27c4cc039c378d16195f99b7c96fe198d41
Moon-ju-young <amymoon07@khu.ac.kr>
2025-08-01 16:17:29 +0900

    ✨ feat: image input 추가
    Resolves: #170
    Related to: #133

--- src/app/mypage/add-activity/_components/ImageUploader.tsx
+++ src/app/mypage/add-activity/_components/ImageUploader.tsx
@@ -1,3 +1,12 @@
+import EyeIcon from '@/assets/icons/eye_off.svg';
+
 export default function ImageUploader() {
-  return;
+  return (
+    <div className='flex gap-12 md:gap-14'>
+      <label className='flex flex-col gap-2 md:gap-10'>
+        <EyeIcon className='size-40 text-gray-400' />
+        <input hidden type='file' />
+      </label>
+    </div>
+  );
 }

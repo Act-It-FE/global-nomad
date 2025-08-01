be6b8504b67e10b800b702bc2bee118cf712c52b
dain <dainbabo@naver.com>
2025-08-01 17:02:15 +0900

    ✨ feat: 로그인 구현

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 import Image from 'next/image';
 import Link from 'next/link';
-import { useRouter } from 'next/router';
+import { useRouter } from 'next/navigation';
 import React, { useState } from 'react';
 
 import authApi from '@/api/authApi';

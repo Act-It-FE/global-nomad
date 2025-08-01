379fa1b93077144f043591180ca502c5925ee47b
Moon-ju-young <amymoon07@khu.ac.kr>
2025-08-01 16:26:54 +0900

    ✨ feat: COMMON_STYLE 상수 추가
    Resolves: #170
    Related to: #133

--- src/app/mypage/add-activity/_components/ImageUploader.tsx
+++ src/app/mypage/add-activity/_components/ImageUploader.tsx
@@ -1,9 +1,15 @@
 import EyeIcon from '@/assets/icons/eye_off.svg';
+import { cn } from '@/utils/cn';
+
+const COMMON_STYLE = cn(
+  'size-80 md:size-126 lg:size-128',
+  'rounded-lg md:rounded-2xl border border-gray-100',
+);
 
 export default function ImageUploader() {
   return (
     <div className='flex gap-12 md:gap-14'>
-      <label className='flex flex-col gap-2 md:gap-10'>
+      <label className={cn(COMMON_STYLE, 'flex flex-col gap-2 md:gap-10')}>
         <EyeIcon className='size-40 text-gray-400' />
         <input hidden type='file' />
       </label>

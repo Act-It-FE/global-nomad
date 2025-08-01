353277094d01f1aa8e8742f14337a0ff5d1985bf
Moon-ju-young <amymoon07@khu.ac.kr>
2025-08-01 17:45:49 +0900

    ✨ feat: modalMessage state 및 Modal component 추가
    Resolves: #170
    Related to: #133

--- src/app/mypage/add-activity/_components/ImageUploader.tsx
+++ src/app/mypage/add-activity/_components/ImageUploader.tsx
@@ -1,8 +1,9 @@
 'use client';
 
-import { ChangeEvent } from 'react';
+import { ChangeEvent, useState } from 'react';
 
 import EyeIcon from '@/assets/icons/eye_off.svg';
+import Modal from '@/components/Modal/Modal';
 import { cn } from '@/utils/cn';
 
 const COMMON_STYLE = cn(
@@ -11,24 +12,43 @@ const COMMON_STYLE = cn(
 );
 
 export default function ImageUploader({ max }: { max: number }) {
+  const [modalMessage, setModalMessage] = useState('');
+
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
-    if (e.target.files?.length && e.target.files?.length > max) return;
+    if (e.target.files?.length && e.target.files?.length > max) {
+      setModalMessage(`파일은 최대 ${max}개까지만 업로드 할 수 있습니다.`);
+      return;
+    }
   };
 
   return (
-    <div className='flex gap-12 md:gap-14'>
-      <label
-        className={cn(
-          COMMON_STYLE,
-          'flex flex-col items-center justify-center gap-2 md:gap-10',
-        )}
-      >
-        <EyeIcon className='size-40 text-gray-400' />
-        <div className='txt-13_M md:txt-14_M leading-[calc(1em+3px)] text-gray-600'>
-          /{max > 0 ? max : 1}
-        </div>
-        <input hidden multiple={max > 1} type='file' onChange={handleChange} />
-      </label>
-    </div>
+    <>
+      <div className='flex gap-12 md:gap-14'>
+        <label
+          className={cn(
+            COMMON_STYLE,
+            'flex flex-col items-center justify-center gap-2 md:gap-10',
+          )}
+        >
+          <EyeIcon className='size-40 text-gray-400' />
+          <div className='txt-13_M md:txt-14_M leading-[calc(1em+3px)] text-gray-600'>
+            /{max > 0 ? max : 1}
+          </div>
+          <input
+            hidden
+            multiple={max > 1}
+            type='file'
+            onChange={handleChange}
+          />
+        </label>
+      </div>
+      {modalMessage && (
+        <Modal
+          message={modalMessage}
+          variant='onlyText'
+          onClose={() => setModalMessage('')}
+        />
+      )}
+    </>
   );
 }

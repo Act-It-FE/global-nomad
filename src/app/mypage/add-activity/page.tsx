b802e32a5197bb953de6a0a8aef574cdcd4a5d03
Moon-ju-young <amymoon07@khu.ac.kr>
2025-08-02 18:31:10 +0900

    ✨ feat: h1 요소 추가
    Resolves: #133

--- src/app/mypage/add-activity/page.tsx
+++ src/app/mypage/add-activity/page.tsx
@@ -7,6 +7,8 @@ export default function Page() {
         'mx-auto max-w-760 px-24 py-30 md:px-30 md:py-40',
         'flex flex-col gap-24',
       )}
-    />
+    >
+      <h1 className='txt-18_B h-41 content-center'>내 체험 등록</h1>
+    </form>
   );
 }

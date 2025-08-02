296423ad08426ec12d1d0657b2753003e64ddeca
Moon-ju-young <amymoon07@khu.ac.kr>
2025-08-02 23:12:05 +0900

    ✨ feat: section 내부 div 요소 추가
    Resolves: #133

--- src/app/mypage/add-activity/page.tsx
+++ src/app/mypage/add-activity/page.tsx
@@ -17,7 +17,12 @@ export default function Page() {
       )}
     >
       <h1 className='txt-18_B h-41 content-center'>내 체험 등록</h1>
-      <section className='flex flex-col gap-30' />
+      <section className='flex flex-col gap-30'>
+        <div className='flex flex-col gap-24' />
+        <div />
+        <div />
+        <div />
+      </section>
       <Button className='w-120' size='sm'>
         {id ? '수정' : '등록'}하기
       </Button>

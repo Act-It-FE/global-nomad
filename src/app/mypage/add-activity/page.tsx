44c413f63de50a909bc5eb1a8213c988b1f337c0
Moon-ju-young <amymoon07@khu.ac.kr>
2025-08-02 18:23:02 +0900

    ♻️ refactor: main > form 태그 변경
    Resolves: #133

--- src/app/mypage/add-activity/page.tsx
+++ src/app/mypage/add-activity/page.tsx
@@ -2,7 +2,7 @@ import { cn } from '@/utils/cn';
 
 export default function Page() {
   return (
-    <main
+    <form
       className={cn(
         'mx-auto max-w-760 px-24 py-30 md:px-30 md:py-40',
         'flex flex-col gap-24',

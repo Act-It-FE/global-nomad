2722121d111dd9117acb7d4a4421c9d1492e143f
Yun-Jinwoo <yjw3480@naver.com>
2025-08-02 20:16:19 +0900

    🎨 style: max-w가 지정되어 있어 gnb가 잘리는 현상 수정

--- src/components/Gnb.tsx
+++ src/components/Gnb.tsx
@@ -41,109 +41,117 @@ export default function Gnb() {
     <nav
       className={cn(
         'sticky top-0 right-0 left-0 z-50 w-full',
-        'flex items-center justify-between',
-        'mx-auto h-48 max-w-1580 px-24 md:h-80 md:px-30',
         'bg-white/60 backdrop-blur-md',
       )}
     >
-      <Link href='/'>
-        <Image
-          alt='logo'
-          className='max-md:hidden'
-          height={28}
-          src={imgLogoText}
-          width={174}
-        />
-        <Image
-          alt='logo'
-          className='md:hidden'
-          height={28}
-          src={imgLogo}
-          width={28}
-        />
-      </Link>
       <div
         className={cn(
-          'txt-14_M flex items-center text-gray-950',
-          user ? 'gap-20' : 'gap-10 md:gap-12',
+          'flex items-center justify-between',
+          'mx-auto h-48 max-w-1580 px-24 md:h-80 md:px-30',
         )}
       >
-        {user ? (
-          <>
-            <div className='relative'>
-              <button type='button' onClick={() => setIsOpen((prev) => !prev)}>
-                {totalCount === 0 ? (
-                  <AlarmInActive
-                    className={cn(
-                      'size-24',
-                      isOpen ? 'text-primary-500' : 'text-gray-600',
-                    )}
-                  />
-                ) : (
-                  <AlarmActive
-                    className={cn(
-                      'size-24',
-                      isOpen ? 'text-primary-500' : 'text-gray-600',
-                    )}
-                  />
-                )}
-              </button>
-              <NotificationPanel
-                fetchNextPage={fetchNextPage}
-                hasNextPage={hasNextPage}
-                isFetchingNextPage={isFetchingNextPage}
-                list={
-                  notifications?.pages.flatMap((page) => page.notifications) ||
-                  []
-                }
-                open={isOpen}
-                totalCount={totalCount}
-                onClose={() => setIsOpen(false)}
-              />
-            </div>
-            <div className='h-14 w-1 bg-gray-100' />
-
-            <DropDown
-              items={[
-                {
-                  text: '마이페이지',
-                  onClick: () =>
-                    router.push(isMobile ? '/mypage' : '/mypage/info'),
-                },
-                {
-                  text: '로그아웃',
-                  danger: true,
-                  onClick: () => {
-                    clearUser();
-                    router.push('/');
+        <Link href='/'>
+          <Image
+            alt='logo'
+            className='max-md:hidden'
+            height={28}
+            src={imgLogoText}
+            width={174}
+          />
+          <Image
+            alt='logo'
+            className='md:hidden'
+            height={28}
+            src={imgLogo}
+            width={28}
+          />
+        </Link>
+        <div
+          className={cn(
+            'txt-14_M flex items-center text-gray-950',
+            user ? 'gap-20' : 'gap-10 md:gap-12',
+          )}
+        >
+          {user ? (
+            <>
+              <div className='relative'>
+                <button
+                  type='button'
+                  onClick={() => setIsOpen((prev) => !prev)}
+                >
+                  {totalCount === 0 ? (
+                    <AlarmInActive
+                      className={cn(
+                        'size-24',
+                        isOpen ? 'text-primary-500' : 'text-gray-600',
+                      )}
+                    />
+                  ) : (
+                    <AlarmActive
+                      className={cn(
+                        'size-24',
+                        isOpen ? 'text-primary-500' : 'text-gray-600',
+                      )}
+                    />
+                  )}
+                </button>
+                <NotificationPanel
+                  fetchNextPage={fetchNextPage}
+                  hasNextPage={hasNextPage}
+                  isFetchingNextPage={isFetchingNextPage}
+                  list={
+                    notifications?.pages.flatMap(
+                      (page) => page.notifications,
+                    ) || []
+                  }
+                  open={isOpen}
+                  totalCount={totalCount}
+                  onClose={() => setIsOpen(false)}
+                />
+              </div>
+              <div className='h-14 w-1 bg-gray-100' />
+              <DropDown
+                items={[
+                  {
+                    text: '마이페이지',
+                    onClick: () =>
+                      router.push(isMobile ? '/mypage' : '/mypage/info'),
                   },
-                },
-              ]}
-              position='bottom'
-              trigger={
-                <div className='flex items-center gap-10'>
-                  <Image
-                    alt='profile image'
-                    className='size-30 rounded-full object-cover'
-                    height={30}
-                    src={user.profileImageUrl ?? imgDefaultProfile}
-                    width={30}
-                  />
-                  {user.nickname}
-                </div>
-              }
-            />
-          </>
-        ) : (
-          <>
-            <div className='w-60 text-center md:w-70'>
-              <Link href='/login'>로그인</Link>
-            </div>
-            <div className='w-60 text-center md:w-70'>
-              <Link href='/signup'>회원가입</Link>
-            </div>
-          </>
-        )}
+                  {
+                    text: '로그아웃',
+                    danger: true,
+                    onClick: () => {
+                      clearUser();
+                      router.push('/');
+                    },
+                  },
+                ]}
+                position='bottom'
+                trigger={
+                  <div className='flex items-center gap-10'>
+                    <Image
+                      alt='profile image'
+                      className='size-30 rounded-full object-cover'
+                      height={30}
+                      src={user.profileImageUrl ?? imgDefaultProfile}
+                      width={30}
+                    />
+                    {user.nickname}
+                  </div>
+                }
+              />
+            </>
+          ) : (
+            <>
+              <div className='w-60 text-center md:w-70'>
+                <Link href='/login'>로그인</Link>
+              </div>
+              <div className='w-60 text-center md:w-70'>
+                <Link href='/signup'>회원가입</Link>
+              </div>
+            </>
+          )}
+        </div>
       </div>
     </nav>
   );

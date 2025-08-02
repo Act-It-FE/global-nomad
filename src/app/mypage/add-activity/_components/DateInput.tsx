b5e675684d615ff4a21523f9059c86f93d1a288f
Moon-ju-young <amymoon07@khu.ac.kr>
2025-08-03 04:38:14 +0900

    ✨ feat: DateInput input date 추가
    Resolves: #133

--- src/app/mypage/add-activity/_components/DateInput.tsx
+++ src/app/mypage/add-activity/_components/DateInput.tsx
@@ -1,3 +1,17 @@
+import Input from '@/components/Input';
+
 export default function DateInput() {
-  return <div className='flex flex-col gap-10 md:flex-row md:gap-14' />;
+  return (
+    <div className='flex flex-col gap-10 md:flex-row md:gap-14'>
+      <div>
+        <label
+          className='txt-16_M mb-10 text-gray-950 max-md:hidden'
+          htmlFor='date'
+        >
+          날짜
+        </label>
+        <Input id='date' type='date-custom' />
+      </div>
+    </div>
+  );
 }

53347b5f7caeb9f920e86b610ca5b02ffc821017
two678 <two6788@gmail.com>
2025-08-04 20:44:23 +0900

    ♻️ refactor: QueryProvider에서 쿼리 클라이언트를 외부로 이동 및 ReactNode로 타입 변경
    Resolves: #230

--- src/app/provider/QueryProvider.tsx
+++ src/app/provider/QueryProvider.tsx
@@ -1,11 +1,11 @@
 'use client';
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { useState } from 'react';
+import { ReactNode } from 'react';
 
-const QueryProvider = ({ children }: { children: React.ReactNode }) => {
-  const [queryClient] = useState(() => new QueryClient());
+export const queryClient = new QueryClient();
 
+const QueryProvider = ({ children }: { children: ReactNode }) => {
   return (
     <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
   );

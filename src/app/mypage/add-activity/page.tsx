9ed0753dea11a4250302e085399843928140dcad
Moon-ju-young <amymoon07@khu.ac.kr>
2025-08-04 09:49:52 +0900

    🎨 style: 너비 스타일 수정
    Resolves: #133

--- src/app/mypage/add-activity/page.tsx
+++ src/app/mypage/add-activity/page.tsx
@@ -177,7 +177,7 @@ export default function Page() {
     <>
       <form
         className={cn(
-          'mx-auto max-w-760 px-24 py-30 md:px-30 md:py-40',
+          'mx-auto w-full max-w-760 px-24 py-30 md:px-30 md:py-40',
           'flex flex-col gap-24',
         )}
         onSubmit={handleSubmit}
